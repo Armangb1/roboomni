ff8746cd1865cebe22743e971da07fc7c0726d0c
armangb1 <arman.ghbn@gmail.com>
2025-06-02 20:37:56 +0330

    add system_bringup launch file

---
+++ src/mecanum_bringup/launch/system_bringup.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    frame_id = LaunchConfiguration('frame_id', default='lidar_scan')
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([
+                    FindPackageShare('rplidar_ros'),
+                    'launch',
+                    'rplidar_c1_launch.py'
+                ])
+            ),
+            launch_arguments={
+                'frame_id': frame_id
+            }.items()
+        ),
+
+        Node(
+            package="kinect_ros2",
+            executable="kinect_ros2_node",
+            name="kinect_ros2",
+            namespace="kinect",
+        ),
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            arguments=["serial", "--dev", "/dev/ttuUSB1"]
+        )
+    ])
