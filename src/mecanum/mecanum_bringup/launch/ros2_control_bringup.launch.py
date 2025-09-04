5efb1a9e2451c7c661d773ebe3cc9fdc02eea82e
Armangb1 <arman.ghbn@gmail.com>
2025-09-04 12:17:29 +0330

    add ros2 control launch

---
+++ src/mecanum/mecanum_bringup/launch/ros2_control_bringup.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+def generate_launch_description():
+
+    config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("mecanum_bringup"),
+            "config",
+            "controllers.yaml"
+        ]
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[config_file]
+    )
+
+
+    forward_command_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_voltage_controller", "--param-file", config_file],
+    )
+
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--param-file", config_file],
+    )
+    return LaunchDescription(
+        [
+            controller_manager,
+            forward_command_controller,
+            joint_state_broadcaster
+        ]
+    )
